8a8eb8bab5324d224dee83588b7a232c221e4c4b
YanWQ-monad <YanWQmonad@gmail.com>
2023-06-19 22:17:10 +0800

    test(csmith): fix 32bit crc

--- tests/csmith/csmith_header_32only.h
+++ tests/csmith/csmith_header_32only.h
@@ -76,10 +76,6 @@ static void crc32_8bytes(uint32_t val) {
     crc32_byte((val >> 8) & 0xff);
     crc32_byte((val >> 16) & 0xff);
     crc32_byte((val >> 24) & 0xff);
-    crc32_byte((val >> 32) & 0xff);
-    crc32_byte((val >> 40) & 0xff);
-    crc32_byte((val >> 48) & 0xff);
-    crc32_byte((val >> 56) & 0xff);
 }
 
 static void transparent_crc(uint32_t val, char vname[], int flag) {
