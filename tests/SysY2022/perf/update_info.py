99c96ab3877598e40db347786b94293026922ee6
YanWQ-monad <YanWQmonad@gmail.com>
2023-07-02 01:12:50 +0800

    ci: support collecting multiple commits at one push

--- tests/SysY2022/perf/update_info.py
+++ tests/SysY2022/perf/update_info.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 
 collected_perf_data_path = sys.argv[1]
-os.makedirs(os.path.join(collected_perf_data_path, 'data'), exist_ok=True)
+os.makedirs(os.path.join(collected_perf_data_path), exist_ok=True)
 
 
 def read_json(file):
@@ -15,17 +15,28 @@ def read_json(file):
         return json.load(f)
 
 
-commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
-commit_time = subprocess.check_output(['git', 'show', '-s', '--format=%ci', 'HEAD']).decode('utf-8').strip()
-commit_msg = subprocess.check_output(['git', 'show', '-s', '--format=%s', 'HEAD']).decode('utf-8').strip()
-commit_parent = subprocess.check_output(['git', 'rev-parse', 'HEAD^']).decode('utf-8').strip()
-
 commit_json_file = os.path.join(collected_perf_data_path, 'commits.json')
 commits_data = read_json(commit_json_file)
-commits_data[commit_hash] = {
-    'time': commit_time,
-    'message': commit_msg,
-    'parent': commit_parent,
-}
+
+for i in range(16):
+    head = f'HEAD~{i}'
+    commit_hash = subprocess.check_output(['git', 'rev-parse', head]).decode('utf-8').strip()
+    if commit_hash in commits_data:
+        break
+
+    commit_time = subprocess.check_output(['git', 'show', '-s', '--format=%ci', head]).decode('utf-8').strip()
+    commit_msg = subprocess.check_output(['git', 'show', '-s', '--format=%s', head]).decode('utf-8').strip()
+    commit_parent = subprocess.check_output(['git', 'rev-parse', f'HEAD~{i+1}']).decode('utf-8').strip()
+
+    print(f'{commit_parent[:7]} -> {commit_hash[:7]}, {commit_msg}')
+
+    commits_data[commit_hash] = {
+        'time': commit_time,
+        'message': commit_msg,
+        'parent': commit_parent,
+    }
+else:
+    print('WARNING: too many commits at one push')
+
 with open(commit_json_file, 'w') as f:
     json.dump(commits_data, f, indent=2, sort_keys=True)
