57a2a7b6c9e9a1c46cbcaf982797b34df3f80656
YanWQ-monad <YanWQmonad@gmail.com>
2023-07-02 23:52:34 +0800

    Fix basePath

--- _next/static/_k8t0JvGGPV1b9AASpkoO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/802-ccbc989970249f49.js","static/chunks/pages/index-38d850628e8f6030.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
