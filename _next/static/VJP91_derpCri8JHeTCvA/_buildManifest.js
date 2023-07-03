c16e22ca5830c3c977de6c59d0cc664bbb873415
YanWQ-monad <YanWQmonad@gmail.com>
2023-07-03 20:32:06 +0800

    Add gcc baseline

--- _next/static/VJP91_derpCri8JHeTCvA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/323-daae2a2249491989.js","static/chunks/pages/index-bd7e3862458597b0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
