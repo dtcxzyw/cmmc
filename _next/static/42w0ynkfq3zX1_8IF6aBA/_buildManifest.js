06b2bda09f1c6b408760ba47d6083744632d2a7a
YanWQ-monad <YanWQmonad@gmail.com>
2023-07-04 23:04:00 +0800

    Add summary

--- _next/static/42w0ynkfq3zX1_8IF6aBA/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/635-5bcabcff0edd6716.js","static/chunks/pages/index-44f23fa12af44fd9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
