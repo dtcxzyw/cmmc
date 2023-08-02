ef576f2eb05f90cf3110c6e42127e43c77f11818
Yingwei Zheng <dtcxzyw2333@gmail.com>
2023-08-03 00:06:27 +0800

    feat(transform): handle scev with loop invariant

--- cmmc/Transforms/Loop/LoopStrengthReduction.cpp
+++ cmmc/Transforms/Loop/LoopStrengthReduction.cpp
@@ -17,10 +17,13 @@
 #include <cmmc/Analysis/LoopAnalysis.hpp>
 #include <cmmc/Analysis/SCEVAnalysis.hpp>
 #include <cmmc/Analysis/SCEVExpr.hpp>
+#include <cmmc/CodeGen/Target.hpp>
+#include <cmmc/IR/Block.hpp>
 #include <cmmc/IR/ConstantValue.hpp>
 #include <cmmc/IR/IRBuilder.hpp>
 #include <cmmc/IR/Instruction.hpp>
 #include <cmmc/Transforms/TransformPass.hpp>
+#include <cmmc/Transforms/Util/PatternMatch.hpp>
 #include <cstdint>
 #include <iostream>
 #include <iterator>
@@ -50,6 +53,116 @@ class LoopStrengthReduction final : public TransformPass<Function> {
         return phi;
     }
 
+    static bool tryExpandSCEV(const SCEVAnalysisResult& scev, const DominateAnalysisResult& dom, Block* block,
+                              Instruction& inst) {
+        if(inst.getInstID() == InstructionID::Add || inst.getInstID() == InstructionID::Phi)
+            return false;
+        auto scevExpr = scev.query(&inst);
+        if(scevExpr && scevExpr->loop && scevExpr->instID == SCEVInstID::AddRec) {
+            if(!dom.dominate(block, scevExpr->loop->latch))
+                return false;
+
+            bool allConstant = true;
+            for(auto& operand : scevExpr->operands) {
+                if(operand->instID != SCEVInstID::Constant) {
+                    allConstant = false;
+                    break;
+                }
+            }
+
+            if(!allConstant)
+                return false;
+            if(scevExpr->operands.empty())
+                return false;
+
+            auto refPhi = scevExpr->loop->header->instructions().front();
+            if(refPhi->getInstID() != InstructionID::Phi)
+                return false;
+            auto refPhiInst = refPhi->as<PhiInst>();
+            // FIXME
+            if(refPhiInst->incomings().size() != 2)
+                return false;
+
+            const auto val = buildAddRec(inst.getType(), refPhiInst, scevExpr, block, inst.asIterator(), 0);
+            inst.replaceWith(val);
+            return true;
+        }
+
+        return false;
+    }
+
+    static bool tryExpandMulAddRecWithLoopInv(const SCEVAnalysisResult& scev, const DominateAnalysisResult& dom, Block* block,
+                                              Instruction& inst, const mir::Target& target, Block* entryBlock) {
+        Value *v1, *v2;
+        if(mul(any(v1), any(v2))(MatchContext<Value>{ &inst })) {
+            Value *initial, *inc, *step = nullptr;
+            const Loop* loop;
+            auto matchAddRec = [&](Value* val) {
+                auto scevExpr = scev.query(val);
+                if(scevExpr && scevExpr->instID == SCEVInstID::AddRec && scevExpr->loop && scevExpr->operands.size() == 2 &&
+                   scevExpr->operands[0]->instID == SCEVInstID::Constant &&
+                   scevExpr->operands[1]->instID == SCEVInstID::Constant) {
+                    loop = scevExpr->loop;
+                    initial = ConstantInteger::get(val->getType(), scevExpr->operands[0]->constant);
+                    inc = ConstantInteger::get(val->getType(), scevExpr->operands[1]->constant);
+                    return true;
+                }
+                return false;
+            };
+            auto matchLoopInvariant = [&](Value* val) {
+                if(val->isConstant())  // should be handled by SCEV
+                    return false;
+                if(!val->getBlock())
+                    return true;
+                if(val->getBlock() == loop->header)
+                    return false;
+                return dom.dominate(val->getBlock(), loop->header);
+            };
+            if(matchAddRec(v1) && matchLoopInvariant(v2)) {
+                step = v2;
+            } else if(matchAddRec(v2) && matchLoopInvariant(v1)) {
+                step = v1;
+            }
+            if(!step)
+                return false;
+            if(!dom.dominate(block, loop->latch))
+                return false;
+
+            auto refPhi = loop->header->instructions().front();
+            if(refPhi->getInstID() != InstructionID::Phi)
+                return false;
+            auto refPhiInst = refPhi->as<PhiInst>();
+            // FIXME
+            if(refPhiInst->incomings().size() != 2)
+                return false;
+
+            IRBuilder builder{ target };
+            if(step->getBlock()) {
+                builder.setInsertPoint(step->getBlock(), std::next(step->as<Instruction>()->asIterator()));
+            } else {
+                builder.setInsertPoint(entryBlock, std::prev(entryBlock->instructions().end()));
+            }
+            const auto base = builder.makeOp<BinaryInst>(InstructionID::Mul, step, initial);
+            const auto increament = builder.makeOp<BinaryInst>(InstructionID::Mul, step, inc);
+            builder.setInsertPoint(refPhi->getBlock(), refPhi->getBlock()->instructions().begin());
+            const auto val = builder.createPhi(base->getType());
+            builder.setInsertPoint(loop->latch, std::prev(loop->latch->instructions().end()));
+            const auto next = builder.makeOp<BinaryInst>(InstructionID::Add, val, increament);
+            for(auto& [pred, incoming] : refPhiInst->incomings()) {
+                if(pred == loop->latch) {
+                    val->addIncoming(pred, next);
+                } else {
+                    val->addIncoming(pred, base);
+                }
+            }
+            inst.replaceWith(val);
+
+            return true;
+        }
+
+        return false;
+    }
+
 public:
     bool run(Function& func, AnalysisPassManager& analysis) const override {
         // auto& loopInfo = analysis.get<LoopAnalysis>(func);
@@ -61,37 +174,13 @@ public:
             auto& instructions = block->instructions();
 
             for(auto& inst : instructions) {
-                if(inst.getInstID() == InstructionID::Add || inst.getInstID() == InstructionID::Phi)
-                    continue;
-                auto scevExpr = scev.query(&inst);
-                if(scevExpr && scevExpr->loop && scevExpr->instID == SCEVInstID::AddRec) {
-                    if(!dom.dominate(block, scevExpr->loop->latch))
-                        continue;
-
-                    bool allConstant = true;
-                    for(auto& operand : scevExpr->operands) {
-                        if(operand->instID != SCEVInstID::Constant) {
-                            allConstant = false;
-                            break;
-                        }
-                    }
-
-                    if(!allConstant)
-                        continue;
-                    if(scevExpr->operands.empty())
-                        continue;
-
-                    auto refPhi = scevExpr->loop->header->instructions().front();
-                    if(refPhi->getInstID() != InstructionID::Phi)
-                        continue;
-                    auto refPhiInst = refPhi->as<PhiInst>();
-                    // FIXME
-                    if(refPhiInst->incomings().size() != 2)
-                        continue;
-
-                    const auto val = buildAddRec(inst.getType(), refPhiInst, scevExpr, block, inst.asIterator(), 0);
-                    inst.replaceWith(val);
+                if(tryExpandSCEV(scev, dom, block, inst)) {
                     modified = true;
+                    continue;
+                }
+                if(tryExpandMulAddRecWithLoopInv(scev, dom, block, inst, analysis.module().getTarget(), func.entryBlock())) {
+                    modified = true;
+                    continue;
                 }
             }
         }
